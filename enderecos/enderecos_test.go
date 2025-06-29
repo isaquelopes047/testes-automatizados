f994a1f0ef8dd88c6870d2da1713a31bfd967087
Isaque Lopes <isaque.lopes047@gmail.com>
2025-06-29 19:25:43 -0300

    testes

--- enderecos/enderecos_test.go
+++ enderecos/enderecos_test.go
@@ -4,15 +4,28 @@ import (
 	"testing"
 )
 
-func TestTipoDeEndreco(t *testing.T) {
-	enderecoParaTeste := "Avenida Paulista"
-	tipoDeEnderecoEsperado := "Avenida"
-	tipoDeEnderecoRecebido := TipoDeEndereco(enderecoParaTeste)
+type cenarioDeTeste struct {
+	enderecoInserido string
+	retornoEsperado  string
+}
 
-	if tipoDeEnderecoRecebido != tipoDeEnderecoEsperado {
-		t.Errorf("Tipo recebido é diferente do esperado! Esperava %s e recebeu %s",
-			tipoDeEnderecoEsperado,
-			tipoDeEnderecoRecebido,
-		)
+func TestTipoDeEndreco(t *testing.T) {
+
+	cenariosDeTeste := []cenarioDeTeste{
+		{"Rua ABC", "Rua"},
+		{"Rua Joao", "Rua"},
+		{"Avenida adão", "Avenida"},
+		{"Rodovia ABC", "Rodovia"},
+		{"Preça ABC", "Tipo Inválido"},
+	}
+
+	for _, cenario := range cenariosDeTeste {
+		tipoDeEnderecoRecebido := TipoDeEndereco(cenario.enderecoInserido)
+		if tipoDeEnderecoRecebido != cenario.retornoEsperado {
+			t.Errorf("Tipo recebido é diferente do esperado! Esperava %s e recebeu %s",
+				tipoDeEnderecoRecebido,
+				cenario.retornoEsperado,
+			)
+		}
 	}
 }
